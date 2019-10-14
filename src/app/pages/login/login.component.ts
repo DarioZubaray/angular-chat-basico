e74a6077c6a51b259c3c29da532382b3f46b9788
dariozubaray <dzubaray@gmail.com>
2019-10-14 17:21:38 -0300

    Creando interfaz de login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { WebsocketService } from 'src/app/services/websocket.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  nombre: string = '';
+  constructor(public wsService: WebsocketService) { }
 
   ngOnInit() {
   }
 
+  ingresar() {
+    this.wsService.loginWS(this.nombre);
+  }
 }
