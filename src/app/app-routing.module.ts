9f4926d6af2c050a313b3c521e357a5abb618fe7
dariozubaray <dzubaray@gmail.com>
2019-10-14 14:57:24 -0300

    creando nuevos componentes a utilizar y dando forma a la vista de mensajes

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { MensajesComponent } from './pages/mensajes/mensajes.component';
+
+const routes: Routes = [
+  {path: '', component: LoginComponent},
+  {path: 'mensajes', component: MensajesComponent},
+  {path: '**', component: LoginComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
