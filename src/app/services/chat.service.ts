017c9b6bc5c26976ec054549b4ebccc958bf624c
dariozubaray <dzubaray@gmail.com>
2019-10-14 18:50:53 -0300

    Completando la lista de usuarios con el servicio rest de usuarios activos

--- src/app/services/chat.service.ts
+++ src/app/services/chat.service.ts
@@ -24,4 +24,8 @@ export class ChatService {
   getMessagesPrivate(){
     return this.wsService.listen('mensaje-privado');
   }
+
+  getUsuariosActivos(){
+    return this.wsService.listen('usuarios-activos');
+  }
 }
